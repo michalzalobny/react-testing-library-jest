807e82221822545e4ca4687f9cadaa4bd58f104a
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-12 11:27:49 +0000

    Switch from fireEvent to useEvent instance for more realistic event based tests

--- sundae-starter/src/pages/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/tests/SummaryForm.test.jsx
@@ -1,9 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { SummaryForm } from "../SummaryForm";
 
 describe("Summary form flow", () => {
-  test("checkbox enables button", () => {
+  test("checkbox enables button", async () => {
     render(<SummaryForm />);
     const checkboxElement = screen.getByRole("checkbox", {
       name: /conditions/i,
@@ -11,17 +12,19 @@ describe("Summary form flow", () => {
     const buttonElement = screen.getByRole("button", { name: /submit/i });
     console.log(checkboxElement);
 
+    const user = userEvent.setup();
+
     // Unchecked by default
     expect(checkboxElement).not.toBeChecked();
 
     // Checking if checkbox enables button
     expect(buttonElement).not.toBeEnabled();
-    fireEvent.click(checkboxElement);
+    await user.click(checkboxElement);
     expect(checkboxElement).toBeChecked();
     expect(buttonElement).toBeEnabled();
 
     // Click checkbox again disabled button
-    fireEvent.click(checkboxElement);
+    await user.click(checkboxElement);
     expect(buttonElement).not.toBeEnabled();
   });
 });
