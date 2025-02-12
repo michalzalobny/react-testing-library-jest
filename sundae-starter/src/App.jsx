eb05b1a8d07a126c98195b76434132cb7d94271a
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-12 18:04:55 +0000

    Add mock server from msw/node and server request reliant tests

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,10 +1,12 @@
-import { SummaryForm } from "./pages/SummaryForm";
+import { SummaryForm } from "./pages/summary/SummaryForm";
+import { Options } from "./pages/entry/Options";
 
 function App() {
   return (
     <div>
       <h1>Sundaes on Demand</h1>
       <SummaryForm />
+      <Options optionType="scoops" />
     </div>
   );
 }
