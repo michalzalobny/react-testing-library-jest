859852e317e8b114443275e0545a655a2ce222fc
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-15 19:17:38 +0000

    Add context to the setup (WIP)

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,13 +1,14 @@
-import { SummaryForm } from "./pages/summary/SummaryForm";
-import { Options } from "./pages/entry/Options";
+import Container from "react-bootstrap/Container";
+import { OrderEntry } from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-      <SummaryForm />
-      <Options optionType="toppings" />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+      </OrderDetailsProvider>
+    </Container>
   );
 }
 
--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,40 +1,48 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 
-export const SummaryForm = () => {
-  const [disabled, setIsDisabled] = useState(true);
+export default function SummaryForm({ setOrderPhase }) {
+  const [tcChecked, setTcChecked] = useState(false);
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-  };
+
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase("completed");
+  }
 
   const popover = (
     <Popover id="popover-basic">
-      <Popover.Body>No ice cream will actually be delivered.</Popover.Body>
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
     </Popover>
   );
 
-  return (
-    <>
-      <div>
-        <form>
-          <button type="submit" disabled={disabled}>
-            Submit
-          </button>
-          <label htmlFor="enable-button">
-            <OverlayTrigger overlay={popover} placement="right">
-              <span>Accept conditions and enable the button</span>
-            </OverlayTrigger>
-          </label>
-          <input
-            type="checkbox"
-            id="enable-button"
-            value={!disabled}
-            onChange={() => setIsDisabled((prev) => !prev)}
-          />
-        </form>
-      </div>
-    </>
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
   );
-};
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+}
